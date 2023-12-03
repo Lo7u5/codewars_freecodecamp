2072b24ec6fdb8ff2053805786c77a626bd6b21c
Lo7u5 <40316422+Lo7u5@users.noreply.github.com>
2023-12-03 09:21:50 +0500

    Create add_item_to_array2.js

---
+++ add_item_to_array2.js
@@ -0,0 +1,8 @@
+function htmlColorNames(arr) {
+
+arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
